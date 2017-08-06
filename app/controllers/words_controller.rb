c35e1076573d628dc27a65c6c8490ba44590abad
wangboguo <wangboguo@wangboguodeMacBook-Pro.local>
2017-08-07 01:06:29 +0800

    记住了和没记住两个方法，ajax调用，改变权重

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,4 +8,24 @@ class WordsController < ApplicationController
     @words = unit.words.order('lower(content)').paginate(page: params[:page], per_page: 30)
     @chapter = unit.chapter
   end
+
+  def remember
+    unit = Unit.find_by_chapter(params[:id])
+    @words = unit.words.order('lower(content)').paginate(page: params[:page], per_page: 30)
+    @chapter = unit.chapter
+    @this_word = Word.find_by_id(params[:word_id])
+    @this_word.weight -= 1
+    @this_word.save unless @this_word.weight < 0
+    redirect_to request.referrer
+  end
+
+  def not_remember
+    unit = Unit.find_by_chapter(params[:id])
+    @words = unit.words.order('lower(content)').paginate(page: params[:page], per_page: 30)
+    @chapter = unit.chapter
+    @this_word = Word.find_by_id(params[:word_id])
+    @this_word.weight += 1
+    @this_word.save unless @this_word.weight < 0
+    redirect_to request.referrer
+  end
 end

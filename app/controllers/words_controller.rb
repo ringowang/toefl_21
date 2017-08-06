9a12f0d258e6980ea1b47362762a50e78ada2427
wangboguo <wangboguo@wangboguodeMacBook-Pro.local>
2017-08-07 03:02:30 +0800

    Version 1 Completed

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,18 +1,15 @@
 class WordsController < ApplicationController
+  before_action :prepare, only: [:show, :remember, :not_remember]
+
   def index
     @words = Word.order('lower(content)').paginate(page: params[:page], per_page: 50)
   end
 
   def show
-    unit = Unit.find_by_chapter(params[:id])
-    @words = unit.words.order('lower(content)').paginate(page: params[:page], per_page: 30)
-    @chapter = unit.chapter
+
   end
 
   def remember
-    unit = Unit.find_by_chapter(params[:id])
-    @words = unit.words.order('lower(content)').paginate(page: params[:page], per_page: 30)
-    @chapter = unit.chapter
     @this_word = Word.find_by_id(params[:word_id])
     if @this_word.weight > 0
       @this_word.weight -= 1
@@ -22,12 +19,32 @@ class WordsController < ApplicationController
   end
 
   def not_remember
-    unit = Unit.find_by_chapter(params[:id])
-    @words = unit.words.order('lower(content)').paginate(page: params[:page], per_page: 30)
-    @chapter = unit.chapter
     @this_word = Word.find_by_id(params[:word_id])
     @this_word.weight += 1
     @this_word.save
     render :show
+  end
+
+  private
+
+  def prepare
+    unit = Unit.find_by_chapter(params[:id])
+    @words = unit.words
+    if params[:status]
+      case params[:status]
+      when 'all'
+        @words = @words
+      when 'never_know'
+        @words = @words.where('weight >= 3')
+      when 'do_not_know'
+        @words = @words.where('weight >= 2')
+      when 'barely_know'
+        @words = @words.where('weight >= 1')
+      when 'got_it'
+        @words = @words.where('weight = 0')
+      end
+    end
+    @words = @words.order('lower(content)').paginate(page: params[:page], per_page: 30)
+    @chapter = unit.chapter
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'words#index'
+  root 'words#show', id: 1
   resources :words, only: [:index, :show] do
     put :remember, on: :member
     put :not_remember, on: :member
